63adb377f3873bf720849c31b1ff5dc409b18f32
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-03-02 20:03:13 +0300

    Task: Created a task

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,17 @@ import React  from 'react';
 import '../App.css';
 import Navbar  from './Navbar';
 import LoginForm from './Login';
+import Task from './Task';
+import TaskList  from './TaskList';
+
+
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <LoginForm />
+      {/* <Navbar /> */}
+      {/* <LoginForm /> */}
+      <Task />
+      <TaskList />
 
 
     </div>
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,9 +2,15 @@ import React from 'react';
 import Task from './Task';
 
 const TaskList = ({ tasks }) => {
+  if (!tasks) {
+    return null; // or render a loading spinner, error message, etc.
+  }
+
   return (
     <div>
-      {tasks.map(task => <Task task={task} key={task.id} />)}
+      {tasks.map(task => (
+        <Task task={task} key={task.id} />
+      ))}
     </div>
   );
 };
