896d1c6cfc681f7ef55fc29adb432a901b7c06b9
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-03-02 22:25:11 +0300

    Created User

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,16 +1,38 @@
 import React, { useState } from "react";
 
-function Signup() {
-  const [user, setUser] = useState({ full_name: "", email: "", password: "" });
+function SignUp() {
+  const [isRegistered, setIsRegistered] = useState(false);
+  const [user, setUser] = useState({ email: "", password: "" });
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUser((prevState) => ({ ...prevState, [name]: value }));
   };
 
-  const handleFormSubmit = (event) => {
+  const handleRegistrationSubmit = (event) => {
     event.preventDefault();
     fetch("http://localhost:9292/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    })
+      .then((response) => {
+        if (response.status === 201) {
+          setIsRegistered(true);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleLoginSubmit = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:9292/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(user),
@@ -20,33 +42,48 @@ function Signup() {
         console.log(data);
       })
       .catch((error) => {
-        console.error("Error:", error);
+        console.log(error);
       });
   };
 
   return (
     <div>
-      <h2>Signup</h2>
-      <form onSubmit={handleFormSubmit}>
-        <label>
-          Full Name:
-          <input type="text" name="full_name" value={user.full_name} onChange={handleInputChange} />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input type="email" name="email" value={user.email} onChange={handleInputChange} />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" name="password" value={user.password} onChange={handleInputChange} />
-        </label>
-        <br />
-        <button type="submit">Sign Up</button>
-      </form>
+      {!isRegistered ? (
+        <form onSubmit={handleRegistrationSubmit}>
+          <label>
+            Full Name:
+            <input type="text" name="full_name" value={user.full_name} onChange={handleInputChange} />
+          </label>
+          <br />
+          <label>
+            Email:
+            <input type="text" name="email" value={user.email} onChange={handleInputChange} />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input type="password" name="password" value={user.password} onChange={handleInputChange} />
+          </label>
+          <br />
+          <button type="submit">Register</button>
+        </form>
+      ) : (
+        <form onSubmit={handleLoginSubmit}>
+          <label>
+            Email:
+            <input type="text" name="email" value={user.email} onChange={handleInputChange} />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input type="password" name="password" value={user.password} onChange={handleInputChange} />
+          </label>
+          <br />
+          <button type="submit">Login</button>
+        </form>
+      )}
     </div>
   );
 }
 
-export default Signup;
+export default SignUp;
